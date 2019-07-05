514b3ea7e1d0be4ae01f231b1dd89baee46797ed
Carlos Perez-Lopez <carlospzlz@gmail.com>
2019-07-05 22:09:35 +0100

    All the big things in place
    TODO:
    Add text with the cost to reduce:
     - time (aka ticks)
     - km

--- simulation.py
+++ simulation.py
@@ -1,36 +1,52 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import collections
+
+import numpy
+from matplotlib import animation
+from matplotlib import pyplot
 
 
-DRONE_MARKER = '^'
+DRONE_MARKER = 'X'
 DRONE_COLOR = 'b'
 CYCLIST_MARKER = 'o'
-CYCLIST_COLOR = 'r'
+CYCLIST_COLOR = 'm'
+DELIVERY_MARKER = 's'
+DELIVERY_COLOR = 'r'
 
 
 class Simulation(object):
     """
     """
-    def __init__(self, schedule, n_drones, n_cyclists):
-        self.__drones = np.zeros(n_drones, dtype=[('position', float, 2)])
-        self.__cyclists = np.zeros(n_cyclists, dtype=[('position', float, 2)])
+    def __init__(self, deliveries, n_drones, n_cyclists, schedule):
+        self.__deliveries = deliveries
+        self.__drones = numpy.zeros(n_drones, dtype=[('position', float, 2)])
+        self.__cyclists = numpy.zeros(
+            n_cyclists, dtype=[('position', float, 2)])
         self.__drones_scatter = None
         self.__cyclists_scatter = None
 
     def start(self):
-        ani = FuncAnimation(
-            plt.gcf(), self.__update, init_func=self.__init_func, interval=10)
-        plt.show()
+        ani = animation.FuncAnimation(
+            pyplot.gcf(), self.__update, init_func=self.__init_func,
+            interval=10)
+        pyplot.show()
 
     def __init_func(self):
-        plt.axis((-20, 20, -20, 20))
-        plt.grid()
-        self.__drones_scatter = plt.scatter(
+        pyplot.axis((-20, 20, -20, 20))
+        pyplot.grid()
+        self.__plotDeliveries()
+        self.__initializeVehicles()
+
+    def __plotDeliveries(self):
+        x_coords = [delivery.destination[0] for delivery in deliveries]
+        y_coords = [delivery.destination[1] for delivery in deliveries]
+        pyplot.plot(x_coords, y_coords, DELIVERY_MARKER + DELIVERY_COLOR)
+
+    def __initializeVehicles(self):
+        self.__drones_scatter = pyplot.scatter(
             [0], [0], marker=DRONE_MARKER, color=DRONE_COLOR)
-        self.__cyclists_scatter = plt.scatter(
+        self.__cyclists_scatter = pyplot.scatter(
             [0], [0], marker=CYCLIST_MARKER, color=CYCLIST_COLOR)
-        plt.legend(
+        pyplot.legend(
             (self.__drones_scatter, self.__cyclists_scatter),
             ('Drones', 'Cyclists'))
 
@@ -50,10 +66,17 @@ class Simulation(object):
         for vehicles, color in trail:
             x = vehicles['position'][:, 0]
             y = vehicles['position'][:, 1]
-            plt.plot(x, y, color + '.', zorder=0)
+            pyplot.plot(x, y, color + '.', zorder=0)
 
 
 if __name__ == '__main__':
-    sim = Simulation(None, 1, 1)
+    Delivery = collections.namedtuple('Delivery', 'packages destination')
+    deliveries = [
+        Delivery(('product0', 'product1', 'product2'), (5, 4)),
+        Delivery(('product3'), (15, 9)),
+        Delivery(('product4'), (6, 7)),
+    ]
+
+    sim = Simulation(deliveries, 1, 1, None)
     sim.start()
 
