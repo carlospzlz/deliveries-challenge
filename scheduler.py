88939b9fe6e0f78ac7a2546290826a2502d7aa63
Carlos Perez-Lopez <carlospzlz@gmail.com>
2019-07-08 19:26:21 +0100

    Made Scheduler ABC

--- scheduler.py
+++ scheduler.py
@@ -1,28 +1,38 @@
 """
+This module contains interface that schedulers should implement.
 """
 
+from abc import ABC, abstractmethod
 from collections import namedtuple
 
 
 Delivery = namedtuple('Delivery', 'packages destination')
 
 
-class Scheduler(object):
+class Scheduler(ABC):
     """
+    Base abstract class for schedulers.
     """
     def __init__(self, name):
         self.__name = name
 
     @property
     def name(self):
+        """
+        Returns the name of the scheduler.
+        """
         return self.__name
 
-    def _get_route_for_drone(self):
+    @abstractmethod
+    def get_route_for_drone(self):
         """
+        Returns route for drone.
         """
         return None
 
-    def _get_route_for_cyclist(self):
+    @abstractmethod
+    def get_route_for_cyclist(self):
         """
+        Returns route for cyclist.
         """
         return None
